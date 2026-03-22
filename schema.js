f96391bada37dce47ca66eeb80ca7165f823b773
bodhekarvarad <bodhekarvarad2004@gmail.com>
2026-03-22 11:58:03 +0530

    replace a export module

--- schema.js
+++ schema.js
@@ -1,19 +1,21 @@
-const Joi=require("joi");
+const Joi = require("joi");
 
-module.exports= listingSchema=Joi.object({
+const listingSchema = Joi.object({
     listing: Joi.object({
         title: Joi.string().required(),
         description: Joi.string().max(500).required(),
-        image: Joi.string().uri().allow(" ",null),
+        image: Joi.string().uri().allow("", null),
         price: Joi.number().min(0).required(),
         country: Joi.string().required(),
         location: Joi.string().max(100).required()
     }).required()
 });
 
-module.exports.reviewSchema=Joi.object({
+const reviewSchema = Joi.object({
     review: Joi.object({
-    rating: Joi.number().required().min(1).max(5),
-    comment: Joi.string().required()
+        rating: Joi.number().required().min(1).max(5),
+        comment: Joi.string().required()
     }).required()
 });
+
+module.exports = { listingSchema, reviewSchema };
