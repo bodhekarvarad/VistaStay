50d35ee5df6eef9739ac739c461cfc12e2b7bd8b
bodhekarvarad <bodhekarvarad2004@gmail.com>
2026-04-14 19:12:10 +0530

    add a login

--- middleware.js
+++ middleware.js
@@ -1,7 +1,17 @@
 module.exports.isLoggedIn=(req,res,next)=>{
-     if(!req.isAuthenticated()){
+   
+    if(!req.isAuthenticated()){
+        //redirect to login page and flash an error message
+        req.session.redirectUrl=req.originalUrl;
      req.flash("error","You must be logged in to create a listing");
      res.redirect('/login');
     }
     next();
 }
+
+module.exports.savedRedirectUrl=(req,res,next)=>{
+    if(req.session.redirectUrl){
+        res.locals.redirectUrl=req.session.redirectUrl;
+    }
+    next();
+};
