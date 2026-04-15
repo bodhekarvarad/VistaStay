9c9bf7d923ffdc801ac8d40603d96f532d990c3f
bodhekarvarad <bodhekarvarad2004@gmail.com>
2026-04-15 18:46:06 +0530

    add a auth to reviews

--- routers/reviews.js
+++ routers/reviews.js
@@ -5,23 +5,10 @@ const  {reviewSchema}  = require('../schema');
 const ExpressError=require('../utils/ExpressError');
 const Listing=require('../models/listing');
 const Review = require('../models/review');
-const validateReview = (req, res, next) => {
-    if (!req.body.review) {
-        throw new ExpressError(400, "Invalid review data");
-    }
-
-    let { error } = reviewSchema.validate(req.body);
-
-    if (error) {
-        let msg = error.details.map(el => el.message).join(",");
-        throw new ExpressError(400, msg);
-    } else {
-        next();
-    }
-};
+const {validateReview, isLoggedIn}=require('../middleware');
 //Reviews 
 //Post route for creating a new review for a listing
-router.post("/",validateReview,wrapAsync(async(req,res)=>{
+router.post("/",isLoggedIn,validateReview,wrapAsync(async(req,res)=>{
   
   let listing=await Listing.findById(req.params.id);
   if (!listing) {
@@ -29,7 +16,9 @@ router.post("/",validateReview,wrapAsync(async(req,res)=>{
         return res.redirect("/listings");
     }  
   let newReview=new Review(req.body.review);
-    listing.reviews.push(newReview);
+  newReview.author=req.user._id;
+  console.log(newReview); 
+  listing.reviews.push(newReview);
    await newReview.save();
     await listing.save();
     req.flash("success","Successfully added a new review");
@@ -40,7 +29,7 @@ router.post("/",validateReview,wrapAsync(async(req,res)=>{
 
 
 //delete route for deleting a review
-router.delete("/:reviewId",wrapAsync(async(req,res)=>{
+router.delete("/:reviewId",isLoggedIn,wrapAsync(async(req,res)=>{
     let {id,reviewId}=req.params;
     // await Listing.findByIdAndUpdate(id,{$pull:{reviews:reviewId}});
    await Review.findByIdAndDelete(reviewId);
