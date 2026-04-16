650bc37f2a5c843d4f7f949f795e12aa24872b6d
bodhekarvarad <bodhekarvarad2004@gmail.com>
2026-04-16 09:18:03 +0530

    insert mvc

--- models/listing.js
+++ models/listing.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const Review = require("./review");
+const { required } = require("joi");
 
 
 
@@ -19,6 +20,7 @@ const listingSchema = new Schema({
 owner:{
   type:mongoose.Schema.Types.ObjectId,
   ref:'User',
+  required:true
 
 },
 });
