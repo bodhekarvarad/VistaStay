ab68c00268b8091b759beb77e8663f94180cffe8
bodhekarvarad <bodhekarvarad2004@gmail.com>
2026-05-03 17:28:21 +0530

    changes in create listing

--- middleware.js
+++ middleware.js
@@ -7,7 +7,7 @@ module.exports.isLoggedIn=(req,res,next)=>{
         //redirect to login page and flash an error message
         req.session.redirectUrl=req.originalUrl;
      req.flash("error","You must be logged in to create a listing");
-     res.redirect('/login');
+      return res.redirect('/login');
     }
     next();
 }
@@ -19,13 +19,25 @@ module.exports.savedRedirectUrl=(req,res,next)=>{
     next();
 };
 
-module.exports.isOwner=async(req,res,next)=>{
-    let {id}=req.params;
+module.exports.isOwner = async (req, res, next) => {
+    if (!req.user) {
+        req.flash("error", "You must be logged in");
+        return res.redirect("/login");
+    }
+
+    let { id } = req.params;
     let listing = await Listing.findById(id);
-        if(!listing.owner.equals(req.user._id)){
-            req.flash("error", "You are not owner");
-            return res.redirect(`/listings/${id}`);
-        }   
+
+    if (!listing) {
+        req.flash("error", "Listing not found");
+        return res.redirect("/listings");
+    }
+
+    if (!listing.owner.equals(req.user._id)) {
+        req.flash("error", "You are not owner");
+        return res.redirect(`/listings/${id}`);
+    }
+
     next();
 };
 
