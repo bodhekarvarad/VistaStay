2045ba29a63e3d2813c7013c9c02ed53422950eb
bodhekarvarad <bodhekarvarad2004@gmail.com>
2026-07-29 11:35:29 +0530

    all things are done

--- init/index.js
+++ init/index.js
@@ -11,50 +11,48 @@ const initData = require("./data.js");
 const mapToken = process.env.MAPBOX_TOKEN;
 
 const geocodingClient = mbxGeocoding({
-    accessToken: mapToken,
+  accessToken: mapToken,
 });
 
-const MONGO_URI = "mongodb://127.0.0.1:27017/wanderlustdb";
+const MONGO_URI = process.env.ATLASDB_URL;
 
 main()
-    .then(() => {
-        console.log("Connected to DB");
-    })
-    .catch((err) => {
-        console.log(err);
-    });
+  .then(() => {
+    console.log("Connected to DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 async function main() {
-    await mongoose.connect(MONGO_URI);
+  await mongoose.connect(MONGO_URI);
 }
 
 const initDB = async () => {
+  await Listing.deleteMany({});
 
-    await Listing.deleteMany({});
-
-    const listingsWithGeometry = await Promise.all(
-        initData.data.map(async (obj) => {
-
-            let response = await geocodingClient
-                .forwardGeocode({
-                    query: obj.location,
-                    limit: 1,
-                })
-                .send();
-
-            return {
-                ...obj,
-
-                owner: "69de2bddfa28adf264acfb93",
-
-                geometry: response.body.features[0].geometry,
-            };
+  const listingsWithGeometry = await Promise.all(
+    initData.data.map(async (obj) => {
+      let response = await geocodingClient
+        .forwardGeocode({
+          query: obj.location,
+          limit: 1,
         })
-    );
+        .send();
 
-    await Listing.insertMany(listingsWithGeometry);
+      return {
+        ...obj,
 
-    console.log("Data initialized");
+        owner: "69de2bddfa28adf264acfb93",
+
+        geometry: response.body.features[0].geometry,
+      };
+    }),
+  );
+
+  await Listing.insertMany(listingsWithGeometry);
+
+  console.log("Data initialized");
 };
 
 initDB();
